5320c0314a7707fde99ead502b039614e65166e2
a-blancoo <angel.skolan@gmail.com>
2020-11-02 13:08:06 +0100

    todays commit

---
+++ server/models/getDate.js
@@ -0,0 +1,8 @@
+/* 
+function getDate() {
+    const date = new Date();
+    const beatyDate = [date.getTime(), '-', date.getDate(), '-', date.getMonth() + 1, '-', date.getFullYear()];
+    return beatyDate;
+}
+export default getDate
+ */
